29733813187880256776b473df668d8cbcce2e67
Ross Stubbs <ross.stubbs@gmail.com>
2019-07-31 16:40:30 +1000

    only tag destination AMI if source AMI has tags

--- copy_encrypted_ami.sh
+++ copy_encrypted_ami.sh
@@ -210,7 +210,9 @@ echo -e "${COLOR}AMI created succesfully in the destination account:${NC} ${CREA
 # Copy Tags
 if [ "${TAG_OPT}x" != "x" ]; then
     AMI_TAGS=$(echo ${AMI_DETAILS} | jq '.Tags')"}"
-    NEW_AMI_TAGS="{\"Tags\":"$(echo ${AMI_TAGS} | tr -d ' ')
-    $(aws ec2 create-tags --resources ${CREATED_AMI} --cli-input-json ${NEW_AMI_TAGS} --profile ${DST_PROFILE} --region ${DST_REGION} || die "Unable to add tags to the AMI in the destination account. Aborting.")
-    echo -e "${COLOR}Tags added sucessfully${NC}"
+    if [ "${AMI_TAGS}x" != "x" ]; then
+        NEW_AMI_TAGS="{\"Tags\":"$(echo ${AMI_TAGS} | tr -d ' ')
+        $(aws ec2 create-tags --resources ${CREATED_AMI} --cli-input-json ${NEW_AMI_TAGS} --profile ${DST_PROFILE} --region ${DST_REGION} || die "Unable to add tags to the AMI in the destination account. Aborting.")
+        echo -e "${COLOR}Tags added sucessfully${NC}"
+    fi
 fi
